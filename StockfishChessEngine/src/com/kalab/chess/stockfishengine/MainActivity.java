81ef6336ba42870ca4b15ac8a78af6f527fa2ee5
gerhard.kalab <gerhard.kalab@gmail.com>
2013-08-28 18:08:01 +0000

    Complete example of a chess engine provider. Provides Stockfish 4 for various cpu targets.

---
+++ StockfishChessEngine/src/com/kalab/chess/stockfishengine/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kalab.chess.stockfishengine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		finish();
+	}
+}
---
+++ StockfishChessEngine/src/com/kalab/chess/stockfishengine/StockfishEngineProvider.java
@@ -0,0 +1,6 @@
+package com.kalab.chess.stockfishengine;
+
+import com.kalab.chess.enginesupport.ChessEngineProvider;
+
+public class StockfishEngineProvider extends ChessEngineProvider {
+}
